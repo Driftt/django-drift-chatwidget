0cf0c896ee8f13e6fdf6910c5451e64226c12163
Lemuel Boyce <lemuelboyce@gmail.com>
2018-07-20 01:10:11 -0400

    cleanups

--- drift_chatwidget/templatetags/drift_chat_widget.py
+++ drift_chatwidget/templatetags/drift_chat_widget.py
@@ -1,11 +1,10 @@
 from django import template
 from django.conf import settings
 
-
 register = template.Library()
 
 
-@register.inclusion_tag('chatwidget/js_snippet.html', takes_context=True, name='chatwidget')
+@register.inclusion_tag('drift_chatwidget/js_snippet.html', takes_context=True, name='drift_chatwidget')
 def chat_widget(context):
     widget_settings = settings.DRIFT_CHAT_WIDGET
     context['drift_widget_code'] = widget_settings.get('ID')
--- setup.py
+++ setup.py
@@ -1,26 +1,13 @@
-import os
 from setuptools import setup
 
-
-def package_files(directory):
-    paths = []
-    for (path, directories, filenames) in os.walk(directory):
-        for filename in filenames:
-            paths.append(os.path.join('..', path, filename))
-    return paths
-
-
-extra_files = package_files('drift_chatwidget')
-
 setup(name='django-drift-chatwidget',
-      version='0.0.1',
+      version='0.1.0',
       description="Easily add Drift's chat widget to your Django apps.",
       long_description=open('README.md').read(),
       author='Lemuel Boyce',
       author_email='lemuelboyce@gmail.com',
-      packages=["drift_chatwidget"],
-      package_data={'': extra_files},
-      url='https://github.com/rhymiz/django-drift-chatwidget',
+      packages=['drift_chatwidget', 'drift_chatwidget.templatetags'],
+      url='https://github.com/rhymiz/django-drift-drift_chatwidget',
       include_package_data=True,
       zip_safe=False,
       license='MIT',
