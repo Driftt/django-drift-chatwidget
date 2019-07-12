d9c11075bc25fa47b7d2d6cc11f79f3081bfe15a
Lemuel Boyce <lemuelboyce@gmail.com>
2019-07-12 13:17:26 -0400

    fix broken home project home page link (#3)
    - added content type to long description
    - bumped version
    
    Signed-off-by: Lemuel Boyce <lemuelboyce@gmail.com>

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup
 
 setup(name='django-drift-chatwidget',
-      version='0.1.1',
+      version='0.1.2',
       description="Easily add Drift's chat widget to your Django apps.",
       long_description=open('README.md').read(),
+      long_description_content_type='text/markdown',
       author='Lemuel Boyce',
       author_email='lemuelboyce@gmail.com',
       packages=['drift_chatwidget', 'drift_chatwidget.templatetags'],
-      url='https://github.com/rhymiz/django-drift-drift_chatwidget',
+      url='https://github.com/Driftt/django-drift-chatwidget',
       include_package_data=True,
       zip_safe=False,
       license='MIT',
